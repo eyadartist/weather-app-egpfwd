8c9327994a55f036f3577623b13af8c1a56bd776
Eyad Farah <45307338+eyadartist@users.noreply.github.com>
2021-02-04 06:10:22 +0200

    add files

---
+++ website/app.js
@@ -0,0 +1,88 @@
+// APT Url: https://openweathermap.org/current#zip
+// API Key: https://home.openweathermap.org/api_keys
+
+// Personal API Key for OpenWeatherMap API
+let baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip='
+let apiKey = ',us&appid=2d3027a75e0626f37d311271909d6d02';
+
+//  Create a new date
+
+let d = new Date();
+let newDate = d.getMonth() + 1 + '/' + d.getDate() + '/' + d.getFullYear();
+console.log(newDate)
+
+// Event listener to add function to existing HTML DOM element
+
+document.getElementById('generate').addEventListener('click', callBack);
+
+/* Function called by event listener */
+
+function callBack(e) {
+    const zipCoder = document.getElementById('zip').value;
+    const content = document.getElementById('feelings').value;
+
+    getWeather(baseURL, zipCoder, apiKey)
+        // New Syntax!
+        .then(function (data) {
+            // Add data
+            console.log(data);
+            postData('/addWeather', {
+                date: newDate,
+                temperature: data.main.temp,
+                content: content
+            })
+        })
+        .then(
+            updateUI()
+        )
+}
+
+/* Function to GET Web API Data*/
+const getWeather = async (baseURL, zipCoder, key) => {
+    const request = await fetch(baseURL + zipCoder + key);
+    try {
+        // Transform into JSON
+        const weatherData = await request.json();
+        return weatherData;
+    }
+    catch (error) {
+        console.log("error", error);
+        // appropriately handle the error
+    }
+};
+
+/* Function to POST data */
+
+const postData = async (url = '', data = {}) => {
+    console.log(data);
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    });
+
+    try {
+        const newData = await response.json();
+        console.log(newData);
+        return newData;
+    } catch (error) {
+        console.log("oh! Looks ther is an error", error)
+    }
+}
+
+//  Update UI
+const updateUI = async () => {
+    const request = await fetch('/all');
+    try {
+        const allData = await request.json();
+        document.getElementById('date').innerHTML = allData.date;
+        document.getElementById('temp').innerHTML = allData.temperature + ' C°';
+        document.getElementById('content').innerHTML = allData.content;
+
+    } catch (error) {
+        console.log("error", error);
+    }
+}
